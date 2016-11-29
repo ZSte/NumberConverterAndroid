1ebb5c91ab3f28b8b71e9702f881f167a8d711b6
ZSte <ZSte@users.noreply.github.com>
2016-11-29 21:37:37 +0100

    Added toolbar with possibility to show the Settings Activity

--- app/src/main/java/com/example/numberconverterandroid/MainActivity.java
+++ app/src/main/java/com/example/numberconverterandroid/MainActivity.java
@@ -4,8 +4,12 @@ package com.example.numberconverterandroid;
  * @author ZSte
  */
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -26,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
 
         t = (TextView) findViewById(R.id.result);
         Spinner from = (Spinner) findViewById(R.id.spinnerFrom);
@@ -78,5 +86,42 @@ public class MainActivity extends AppCompatActivity {
         num = Integer.parseInt(userInput.getText().toString());
         convNum = Integer.parseInt(c.convert(num, fromN, toN));
         t.setText(getString(R.string.yourResult) + " " + convNum);
+    }
+
+    /**
+     * Creates the Menu in the toolbar
+     * @param menu actual menu
+     * @return returns true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    /**
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                this.startActivity(intent);
+                return true;
+
+            /*case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;*/
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
